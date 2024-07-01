8e39c45efffe37b73729b521c87b265ae90646d0
Purvika Jagtap <purvikajagtap@gmail.com>
2024-07-01 20:10:40 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -10,7 +10,7 @@ export const LANGUAGE_VERSIONS = {
 export const CODE_SNIPPETS = {
   javascript: `\nfunction greet(name) {\n\tconsole.log("Hello, " + name + "!");\n}\n\ngreet("Alex");\n`,
   typescript: `\ntype Params = {\n\tname: string;\n}\n\nfunction greet(data: Params) {\n\tconsole.log("Hello, " + data.name + "!");\n}\n\ngreet({ name: "Alex" });\n`,
-  python: `\ndef greet(name):\n\tprint("Hello, " + name + "!")\n\ngreet("Alex")\n`,
+  python: `\ndef greet(name):\n\tprint("Hello, " + name + "!")\n\ngreet("Purvika Jagtap")\n`,
   java: `\npublic class HelloWorld {\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println("Hello World");\n\t}\n}\n`,
   csharp:
     'using System;\n\nnamespace HelloWorld\n{\n\tclass Hello { \n\t\tstatic void Main(string[] args) {\n\t\t\tConsole.WriteLine("Hello World in C#");\n\t\t}\n\t}\n}\n',
